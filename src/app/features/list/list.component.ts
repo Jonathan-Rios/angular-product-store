5211ce7343f088166ada505cbd70619e503528da
Jonathan <jonathan.riosousa@gmail.com>
2024-07-15 07:23:27 -0300

    feature: add no items message on product list

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -11,17 +11,19 @@ import {
 } from '@angular/material/dialog';
 import { filter } from 'rxjs';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
   products = signal<Product[]>(
-    inject(ActivatedRoute).snapshot.data['products']
+    // inject(ActivatedRoute).snapshot.data['products']
+    []
   );
 
   productsService = inject(ProductsService);
